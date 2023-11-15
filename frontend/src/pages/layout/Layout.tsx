8cf8bc4ba420ac1a4b4ca403e4be6d40449918a9
Patrick Dreker <patrick.dreker@codecamp-n.com>
2023-11-15 17:18:04 +0100

    Update Header

--- frontend/src/pages/layout/Layout.tsx
+++ frontend/src/pages/layout/Layout.tsx
@@ -1,6 +1,6 @@
 import { Outlet, Link } from "react-router-dom";
 import styles from "./Layout.module.css";
-import Azure from "../../assets/Azure.svg";
+import CodeCamp from "../../assets/CodeCamp.svg";
 import { CopyRegular, ShareRegular } from "@fluentui/react-icons";
 import { CommandBarButton, Dialog, Stack, TextField, ICommandBarStyles, IButtonStyles, DefaultButton } from "@fluentui/react";
 import { useContext, useEffect, useState } from "react";
@@ -74,7 +74,7 @@ const Layout = () => {
                 >
                     <Stack horizontal verticalAlign="center">
                         <img
-                            src={Azure}
+                            src={CodeCamp}
                             className={styles.headerIcon}
                             aria-hidden="true"
                         />
